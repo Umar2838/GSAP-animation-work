fc4a5fd4e8685749f05286c3e559ca9a52282513
Umar Aqeel <112412509+Umar2838@users.noreply.github.com>
2024-05-03 20:30:44 +0500

    gsap understanding

---
+++ script.js
@@ -0,0 +1,46 @@
+// var tl =  gsap.timeline()
+// tl.from(".logo",{
+//     y:20,
+//     opacity:0,
+//     duration:1,
+// })
+// tl.from(".nav-items div",{
+//     y:20,
+//     duration:1,
+//     opacity:0,
+//     stagger:0.5
+// })
+// tl.from("h1",{
+// scale:0.5,
+// opacity:0,
+// duration:2
+// })
+
+// gsap.from(".cards",{
+// y:-80,
+// stagger:1,
+// duration:2,
+// scrollTrigger:{
+//     trigger:".cards",
+//     scroller:"body",
+//     start:"top 60%",
+//     end:"top 30%",
+//     scrub:5
+// }
+
+// })
+
+// gsap.to(".page3 h2",{
+//     transform:"translateX(-100%)",
+//     scrollTrigger:{
+//         trigger:".page3",
+//         scroller:"body",
+//         markers:true,
+//         start:"top 0%",
+//         end:"top -100%",
+//         scrub:3,
+//         pin:true
+//     }
+// })
+
+var initialpath =  `M 10 80 Q 250 10 490 80`
